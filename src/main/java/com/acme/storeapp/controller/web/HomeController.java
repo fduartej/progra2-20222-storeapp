083da364306b345de987fc69ce279b9ca4b4af56
Fred Duarte <fduartej@gmail.com>
2022-10-01 13:52:10 -0500

    APIS con rest template y libreria de sendgrid

--- src/main/java/com/acme/storeapp/controller/web/HomeController.java
+++ src/main/java/com/acme/storeapp/controller/web/HomeController.java
@@ -8,14 +8,28 @@ import org.springframework.validation.BindingResult;
 
 import javax.validation.Valid;
 
+import com.acme.storeapp.integration.sendgrid.*;
+import com.acme.storeapp.integration.spotity.*;
 
 @Controller
 public class HomeController {
     
     private static final String HOME_INDEX ="welcome"; 
 
+    private final SpotifyAPI spotifyAPI;
+    private final SendGridAPI sendGridAPI;
+
+    public HomeController(SpotifyAPI spotifyAPI,SendGridAPI sendGridAPI ){
+        this.spotifyAPI = spotifyAPI;
+        this.sendGridAPI = sendGridAPI;
+    }
+
+
     @GetMapping("/")
     public String index(Model model) {
+        
+        spotifyAPI.me();
+        sendGridAPI.send("fduartej@usmp.pe","fduartej@gmail.com","test","test");
         return HOME_INDEX;
     }
 
---
+++ src/main/java/com/acme/storeapp/integration/spotity/dto/Followers.java
@@ -0,0 +1,14 @@
+package com.acme.storeapp.integration.spotity.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Followers{
+
+    private int total; 
+
+}
